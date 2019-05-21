58c47f473ebf4215f0700f6036e99b88ded9a9df
Andrew Azores <me@andrewazor.es>
2019-05-21 17:39:46 -0400

    Hide dump component when not connected to any target JVM

--- src/app/dump/dump-button.component.ts
+++ src/app/dump/dump-button.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommandChannelService } from '../command-channel.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dump-button',
   templateUrl: './dump-button.component.html'
 })
-export class DumpButtonComponent {
+export class DumpButtonComponent implements OnInit, OnDestroy {
   name = '';
   duration = -1;
   events = '';
+  connected = false;
+
+  private readonly subscriptions: Subscription[] = [];
 
   constructor(
     public svc: CommandChannelService,
   ) { }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.svc.onResponse('is-connected')
+        .subscribe(r => this.connected = r.payload === 'true')
+    );
+
+    this.subscriptions.push(
+      this.svc.onResponse('disconnect')
+        .subscribe(() => this.connected = false)
+    );
+
+    this.subscriptions.push(
+      this.svc.onResponse('connect')
+        .subscribe(() => this.connected = true)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 
   dump(): void {
     if (this.duration > 0) {
--- src/app/dump/dump-button.module.ts
+++ src/app/dump/dump-button.module.ts
@@ -1,9 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DumpButtonComponent } from './dump-button.component';
 
 @NgModule({
   imports: [
+    BrowserModule,
     FormsModule,
   ],
   declarations: [
