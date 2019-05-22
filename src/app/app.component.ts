22a54b1d34dbc9dfb287c4629a9bb0a190ded43d
Andrew Azores <me@andrewazor.es>
2019-05-21 22:23:27 -0400

    Remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommandChannelService } from './command-channel.service';
 
 @Component({
