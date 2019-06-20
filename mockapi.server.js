21e23a7cb4365978273792e1736e096d07f7e1dc
Andrew Azores <me@andrewazor.es>
2019-06-20 11:03:11 -0400

    Add /upload and /uploadurl endpoints for testing

--- mockapi.server.js
+++ mockapi.server.js
@@ -11,6 +11,21 @@ app.get('/clienturl', (req, res) => {
     }));
 });
 
+app.get('/uploadurl', (req, res) => {
+  res
+    .status(200)
+    .type('application/json')
+    .send(JSON.stringify({
+      uploadUrl: `http://localhost:${port}/upload`
+    }));
+});
+
+app.post('/upload', (req, res) => {
+  res
+    .status(200)
+    .send('OK');
+});
+
 app.listen(port, () => {
   console.log(`MockAPI server listening on port ${port}`);
 });
