d0b6168fa5c6c9fba98d646cb1ef8b9dfbc76bc3
Andrew Azores <me@andrewazor.es>
2019-08-08 15:29:56 -0400

    Include git tag in built image tag

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-docker build . -t quay.io/rh-jmc-team/container-jfr-web
+gittag="$(git describe --abbrev=0 | tr -d v)"
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ "$branch" == "master" ]; then
+    version="${gittag}"
+else
+    version="${gittag}-${branch}"
+fi
+
+tag="quay.io/rh-jmc-team/container-jfr-web:${version}"
+
+echo "Building to ${tag}..."
+
+docker build . -t "${tag}"
