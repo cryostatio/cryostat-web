27a731f485975b3e10e50676da132bccf7b0cef7
kxu <kxu@redhat.com>
2020-01-15 17:51:01 -0500

    refactor Accepted-Language header building process

--- src/app/documentation-messages.service.ts
+++ src/app/documentation-messages.service.ts
@@ -16,33 +16,14 @@ export class DocumentationMessagesService {
     private http: HttpClient,
     private notifications: NotificationService,
   ) {
-    this.setLocale(navigator.language);
+    this.setLocale(navigator.language); // use the UA language by default
   }
 
-  setLocale(locale: string = navigator.language) {
-    const languages = navigator.languages.filter((v) => v !== locale);
-    languages.unshift(locale);
-    const locales = [];
-    languages.forEach(lang => {
-      if (lang.indexOf('-') === -1) {
-        locales.push(lang);
-      } else {
-        locales.push(lang);
-        locales.push(lang.split('-')[0]);
-      }
-    });
-    const range = locales.map((v, i) => {
-      if (i > 10) {
-        i = 10;
-      }
-      return `${v};q=${1 - i * 0.1}`;
-    }).join(',');
-
-
+  setLocale(locale: string) {
     this.ready.next(false);
     this.http.get('/documentation_messages', {
       headers: {
-        'Accepted-Language': range
+        'Accepted-Language': this.buildAcceptedLanguage(locale)
       }
     }).subscribe(
       (dictionary: object) => {
@@ -54,6 +35,36 @@ export class DocumentationMessagesService {
           'Failed to fetch documentation messages', res.error.message, false, null, null);
       }
     );
+  }
+
+  private buildAcceptedLanguage(locale: string): string {
+    const languages = navigator.languages.filter((v) => v !== locale).filter((v) => v !== locale);
+    languages.unshift(locale); // ordered by priority
+
+    // Include base languages as fallback. eg. en-US => en-US, en
+    const locales = [];
+    languages.forEach(lang => {
+      if (lang.indexOf('-') === -1) {
+        locales.push(lang);
+      } else {
+        locales.push(lang);
+        locales.push(lang.split('-')[0]);
+      }
+    });
+
+    // Append q-factor weights, randing from 0 to 1 inclusive with 1 being the highest priority, to each locale. See RFC-7231#5.3.5
+    return locales
+      .filter((v, i) => locales.indexOf(v) === i) // remove duplicates
+      .map((l, i) => {
+      const q = 1 - i * 0.1;
+      if (q === 1) {
+        return l; // ';q=1' can be omitted
+      } else if (q <= 0) {
+        return l + ';q=0';
+      } else {
+        return l + `;q=` + q;
+      }
+    }).join(',');
   }
 
   isReady(): Observable<boolean> {
