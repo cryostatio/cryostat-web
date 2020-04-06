2494265dd903e155b4bf8b4164cb5fd1c9e9136b
Andrew Azores <me@andrewazor.es>
2020-04-06 07:48:13 -0400

    Initial rewrite commit using patternfly-react-seed

---
+++ src/app/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { PageSection, Title } from '@patternfly/react-core';
+
+const Dashboard: React.FunctionComponent<{}> = () => (
+    <PageSection>
+      <Title size="lg">Dashboard Page Title</Title>
+    </PageSection>
+  )
+
+export { Dashboard };
