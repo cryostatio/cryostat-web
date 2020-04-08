8f90417c74c441071d657c525a131d1fd5f8ac2a
Andrew Azores <me@andrewazor.es>
2020-04-08 17:51:05 -0400

    Add component for (dis)connecting targets

--- src/app/AppLayout/AppLayout.tsx
+++ src/app/AppLayout/AppLayout.tsx
@@ -35,7 +35,7 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({children}) => {
   };
   const Header = (
     <PageHeader
-      logo="Patternfly"
+      logo="ContainerJFR"
       logoProps={logoProps}
       toolbar="Toolbar"
       showNavToggle
--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
+import { TargetSelect } from '@app/TargetSelect/TargetSelect';
 
 export class Dashboard extends React.Component {
 
   render() {
     return (
       <PageSection>
-        <Title size="lg">Dashboard Page Title</Title>
+        <Title size="lg">Dashboard</Title>
+        <TargetSelect />
       </PageSection>
     );
   }
