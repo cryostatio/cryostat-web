662b3fee905bc239816822e04b5df4ea4f651a1c
Andrew Azores <me@andrewazor.es>
2020-04-09 13:39:18 -0400

    Add rudimentary recording table view

--- src/app/RecordingList/RecordingList.tsx
+++ src/app/RecordingList/RecordingList.tsx
@@ -1,13 +1,82 @@
 import * as React from 'react';
-import { Grid, GridItem } from '@patternfly/react-core';
+import { filter, map } from 'rxjs/operators';
+import { Grid, GridItem, PageSection, Title } from '@patternfly/react-core';
+import { Table, TableHeader, TableBody, textCenter } from '@patternfly/react-table';
 import { ServiceContext } from '@app/Shared/Services/Services';
+
+interface Recording {
+  id: number;
+  name: string;
+  state: RecordingState;
+  startTime: number;
+  duration: number;
+  continuous: boolean;
+  toDisk: boolean;
+  maxSize: number;
+  maxAge: number;
+  downloadUrl: string;
+  reportUrl: string;
+}
+
+enum RecordingState {
+  STOPPED = 'STOPPED',
+  STARTING = 'STARTING',
+  RUNNING = 'RUNNING',
+  STOPPING = 'STOPPING',
+}
 
 export const RecordingList = (props) => {
   const context = React.useContext(ServiceContext);
 
+  const [recordings, setRecordings] = React.useState([]);
+
+  const tableColumns: string[] = [
+    'Name',
+    'Start Time',
+    'Duration',
+    'Download',
+    'Report',
+    'State',
+  ];
+
+  const getRecordingRows = () => {
+    return recordings.map((recording: Recording) => [
+      recording.name,
+      new Date(recording.startTime).toISOString(),
+      `${recording.duration / 1000} s`,
+      recording.downloadUrl,
+      recording.reportUrl,
+      recording.state,
+    ]);
+  };
+
+  React.useEffect(() => {
+    const sub = context.commandChannel.onResponse('list')
+      .pipe(
+        filter(m => m.status === 0),
+        map(m => m.payload),
+      )
+      .subscribe(recordings => setRecordings(recordings));
+    return () => sub.unsubscribe();
+  }, []);
+
+  React.useEffect(() => {
+    context.commandChannel.sendMessage('list');
+    const id = setInterval(() => context.commandChannel.sendMessage('list'), 5000);
+    return () => clearInterval(id);
+  }, []);
+
   return (
-    <div>
-      Recording List
-    </div>
+    <PageSection>
+      <Grid>
+        <GridItem span={12}>
+          <Title size="lg">JDK Flight Recordings</Title>
+          <Table aria-label="Recordings Table" cells={tableColumns} rows={getRecordingRows()}>
+            <TableHeader />
+            <TableBody />
+          </Table>
+        </GridItem>
+      </Grid>
+    </PageSection>
   );
 };
