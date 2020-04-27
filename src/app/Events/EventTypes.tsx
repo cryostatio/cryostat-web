80ae1366a25c901c724a660b656d0bb109287c25
Andrew Azores <me@andrewazor.es>
2020-04-27 14:59:46 -0400

    Add event-types table pagination

--- src/app/Events/EventTypes.tsx
+++ src/app/Events/EventTypes.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { filter, map } from 'rxjs/operators';
+import { Pagination } from '@patternfly/react-core';
 import { Table, TableBody, TableHeader } from '@patternfly/react-table';
 import { ServiceContext } from '@app/Shared/Services/Services';
 
@@ -21,6 +22,9 @@ export const EventTypes = (props) => {
   const context = React.useContext(ServiceContext);
 
   const [types, setTypes] = React.useState([]);
+  const [displayedTypes, setDisplayedTypes] = React.useState([]);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [perPage, setPerPage] = React.useState(4);
 
   const tableColumns = [
     'Name',
@@ -29,12 +33,6 @@ export const EventTypes = (props) => {
     'Categories',
     'Options',
   ];
-
-  const getEventTypes = () => {
-    return types.map((t: EventType) => {
-      return [ t.name, t.typeId, t.description, t.category.join(', ').trim(), JSON.stringify(t.options) ];
-    })
-  }
 
   React.useEffect(() => {
     const sub = context.commandChannel.onResponse('list-event-types')
@@ -50,12 +48,38 @@ export const EventTypes = (props) => {
     context.commandChannel.sendMessage('list-event-types');
   }, []);
 
+  React.useEffect(() => {
+    const offset = (currentPage - 1) * perPage;
+    const page = types.slice(offset, offset + perPage);
+    setDisplayedTypes(page);
+  }, [currentPage, perPage, types]);
+
+  const getEventTypes = () => {
+    return displayedTypes.map((t: EventType) => {
+      return [ t.name, t.typeId, t.description, t.category.join(', ').trim(), JSON.stringify(t.options) ];
+    })
+  };
+
+  const onCurrentPage = (evt, currentPage) => setCurrentPage(currentPage);
+
+  const onPerPage = (evt, perPage) => setPerPage(perPage);
+
   // TODO paginate or make scrollable, and put Options into collapsible rows
-  return(
+  return(<>
+    <Pagination
+      itemCount={types.length}
+      page={currentPage}
+      perPage={perPage}
+      onSetPage={onCurrentPage}
+      widgetId="event-types-pagination"
+      onPerPageSelect={onPerPage}
+      perPageOptions={[ { title: '4', value: 4 }, { title: '10', value: 10 }, { title: '20', value: 20 } ]}
+      isCompact
+    />
     <Table aria-label="Event Types table" cells={tableColumns} rows={getEventTypes()}>
       <TableHeader />
       <TableBody />
     </Table>
-  );
+  </>);
 
 }
