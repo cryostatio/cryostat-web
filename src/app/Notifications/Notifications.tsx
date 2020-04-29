9e2212fa07f30189888049332876eaad5dd52078
Andrew Azores <me@andrewazor.es>
2020-04-29 15:52:34 -0400

    Add global toast notifications

---
+++ src/app/Notifications/Notifications.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Observable, Subject } from 'rxjs';
+import { AlertVariant } from '@patternfly/react-core';
+import { nanoid } from 'nanoid';
+
+interface Notification {
+  key?: string;
+  title: string;
+  message?: string;
+  variant: AlertVariant;
+  timeout?: number;
+}
+
+const DefaultNotificationTimeout = 15_000;
+
+class Notifications {
+
+  private readonly notifications = new Subject<Notification>();
+
+  notify(notification: Notification): void {
+    if (!notification.key) {
+      notification.key = nanoid();
+    }
+    if (notification.timeout == undefined || notification.timeout < 0) {
+      notification.timeout = DefaultNotificationTimeout;
+    }
+    this.notifications.next(notification);
+  }
+
+  success(title: string, message?: string, timeout?: number): void {
+    this.notify({ title, message, timeout, variant: AlertVariant.success });
+  }
+
+  info(title: string, message?: string, timeout?: number): void {
+    this.notify({ title, message, timeout, variant: AlertVariant.info });
+  }
+
+  warning(title: string, message?: string, timeout?: number): void {
+    this.notify({ title, message, timeout, variant: AlertVariant.warning });
+  }
+
+  danger(title: string, message?: string, timeout?: number): void {
+    this.notify({ title, message, timeout, variant: AlertVariant.danger });
+  }
+
+  watch(): Observable<Notification> {
+    return this.notifications.asObservable();
+  }
+
+}
+
+const NotificationsContext = React.createContext(new Notifications());
+
+export { Notification, DefaultNotificationTimeout, Notifications, NotificationsContext };
