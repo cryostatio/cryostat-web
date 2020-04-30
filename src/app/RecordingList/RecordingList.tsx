d522da71ffddf0da491d5a11d4ebd03a72151702
Andrew Azores <me@andrewazor.es>
2020-04-30 11:38:16 -0400

    Replace React Table with Data List for flexibility
    Render recording download and report URLs as downloadable links

--- src/app/RecordingList/RecordingList.tsx
+++ src/app/RecordingList/RecordingList.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import { filter, map } from 'rxjs/operators';
-import { Button, Card, CardBody, CardHeader, PageSection, Text, TextVariants, Title } from '@patternfly/react-core';
-import { Table, TableHeader, TableBody, textCenter } from '@patternfly/react-table';
+import { Button, Card, CardBody, CardHeader, DataList, DataListItem, DataListItemRow, DataListItemCells, DataListCell, PageSection, Text, TextVariants, Title } from '@patternfly/react-core';
 import { ServiceContext } from '@app/Shared/Services/Services';
 import { TargetView } from '@app/TargetView/TargetView';
 
@@ -74,16 +73,74 @@ export const RecordingList = (props) => {
     return () => clearInterval(id);
   }, []);
 
+  const RecordingRow = (props) => {
+    return (
+      <DataListItemRow>
+        <DataListItemCells
+          dataListCells={[
+            <DataListCell key={`table-row-${props.index}-1`}>
+              {props.recording.name}
+            </DataListCell>,
+            <DataListCell key={`table-row-${props.index}-2`}>
+              <ISOTime timeStr={props.recording.startTime} />
+            </DataListCell>,
+            <DataListCell key={`table-row-${props.index}-3`}>
+              <RecordingDuration duration={props.recording.duration} />
+            </DataListCell>,
+            <DataListCell key={`table-row-${props.index}-4`}>
+              <Link download url={props.recording.downloadUrl} />
+            </DataListCell>,
+            // TODO make row expandable and render report in collapsed iframe
+            <DataListCell key={`table-row-${props.index}-5`}>
+              <Link url={props.recording.reportUrl} />
+            </DataListCell>,
+            <DataListCell key={`table-row-${props.index}-6`}>
+              {props.recording.state}
+            </DataListCell>
+          ]}
+        />
+      </DataListItemRow>
+    );
+  };
+
+  const ISOTime = (props) => {
+    const fmt = new Date(props.timeStr).toISOString();
+    return (<span>{fmt}</span>);
+  };
+
+  const RecordingDuration = (props) => {
+    const str = props.duration === 0 ? 'Continuous' : `${props.duration / 1000}s`
+    return (<span>{str}</span>);
+  };
+
+  const Link = (props) => {
+    return (<a href={props.url} download={!!props.download} target="_blank">{props.display || props.url}</a>);
+  };
+
   return (
     <TargetView pageTitle="Recordings">
       <Card>
         <CardHeader><Text component={TextVariants.h4}>Active Recordings</Text></CardHeader>
         <CardBody>
-          <Button onClick={handleCreateRecording} >Create</Button>
-          <Table aria-label="Recordings Table" cells={tableColumns} rows={getRecordingRows()}>
-            <TableHeader />
-            <TableBody />
-          </Table>
+          <Button onClick={handleCreateRecording}>Create</Button>
+          <DataList aria-label="Recording List">
+            <DataListItem aria-labelledby="table-header-1">
+              <DataListItemRow>
+                <DataListItemCells
+                  dataListCells={tableColumns.map((key , idx) => (
+                    <DataListCell key={key}>
+                      <span id={`table-header-${idx}`}>{key}</span>
+                    </DataListCell>
+                  ))}
+                />
+              </DataListItemRow>
+            </DataListItem>
+            <DataListItem aria-labelledby="table-row-1-1">
+            {
+              recordings.map((r, idx) => <RecordingRow recording={r} index={idx}/>)
+            }
+            </DataListItem>
+          </DataList>
         </CardBody>
       </Card>
     </TargetView>
