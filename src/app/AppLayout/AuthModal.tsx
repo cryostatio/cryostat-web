8ccdd5b361f10d9b3137218fb1728efe5e73ce55
Andrew Azores <me@andrewazor.es>
2021-03-12 21:09:45 +0000

    Use URL-safe Base64 encoding without padding (#173)
    * Use URL-safe Base64 encoding without padding
    
    * Add license header due to npm run format

--- src/app/AppLayout/AuthModal.tsx
+++ src/app/AppLayout/AuthModal.tsx
@@ -38,6 +38,7 @@
 import * as React from 'react';
 import { first } from 'rxjs/operators';
 import { ActionGroup, Button, Form, FormGroup, Modal, ModalVariant, TextInput } from '@patternfly/react-core';
+import { Base64 } from 'js-base64';
 import { ServiceContext } from '@app/Shared/Services/Services';
 
 export interface AuthModalProps {
@@ -60,7 +61,7 @@ export const AuthModal: React.FunctionComponent<AuthModalProps> = (props) => {
 
   const handleSave = () => {
     context.target.target().pipe(first()).subscribe(target => {
-      context.target.setCredentials(target.connectUrl, window.btoa(`${username}:${password}`));
+      context.target.setCredentials(target.connectUrl, Base64.encodeURL(`${username}:${password}`));
       context.target.setAuthRetry();
       clear();
       props.onSave();
