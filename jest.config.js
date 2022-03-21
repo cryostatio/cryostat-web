345921b2f07e223a294ed4d84c5ed2b78caa62f1
Hareet Dhillon <hdhillon@redhat.com>
2022-03-21 12:19:25 -0400

    Finalize testing configurations for local and CI environments. Delegate code coverage output decision to these configurations

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,6 @@ module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
