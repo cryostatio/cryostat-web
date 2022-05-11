743ce69044143d34e78272f8339ca5195d045562
Janelle Law <jalaw@redhat.com>
2022-05-11 14:22:41 -0400

    chore(tests): add recording table snapshots

---
+++ src/test/Recordings/ArchivedRecordingsTable.test.tsx
@@ -0,0 +1,129 @@
+/*
+ * Copyright The Cryostat Authors
+ * 
+ * The Universal Permissive License (UPL), Version 1.0
+ * 
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ * 
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software (each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ * 
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ * 
+ * This license is subject to the following condition:
+ * The above copyright notice and either this complete permission notice or at
+ * a minimum a reference to the UPL must be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import * as React from 'react';
+import renderer, { act } from 'react-test-renderer'
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom';
+import { of } from 'rxjs';
+import { ServiceContext, defaultServices } from '@app/Shared/Services/Services'
+
+jest.mock('@app/Recordings/RecordingsTable', () => {
+  return {
+    RecordingsTable: jest.fn((props) => {
+      return <div>
+                {props.tableTitle}
+             </div>
+    })
+  };
+});
+
+jest.mock('@app/Shared/Services/Api.service', () => {
+  return {
+    ApiService: jest.fn(() => {
+      return {
+        deleteArchivedRecording: jest.fn((name: string) => {
+            return of(true);
+        }),
+        graphql: jest.fn((query: string) => {
+            return of(true);
+        }),
+      };
+    })
+  };
+});
+
+jest.mock('@app/Shared/Services/NotificationChannel.service', () => {
+  return {
+    ...jest.requireActual('@app/Shared/Services/NotificationChannel.service'),
+    NotificationChannel: jest.fn(() => {
+      return {
+        messages: jest
+          .fn()
+          .mockReturnValue(of()),
+      };
+    }),
+  };
+});
+
+jest.mock('@app/Shared/Services/Target.service', () => {
+    return {
+      ...jest.requireActual('@app/Shared/Services/Target.service'),
+      TargetService: jest.fn(() => {
+        return {
+          deleteCredentials: jest.fn(),
+        };
+      }),
+    };
+  });
+
+  jest.mock('@app/Shared/Services/Report.service', () => {
+    return {
+      ...jest.requireActual('@app/Shared/Services/Report.service'),
+      delete: jest.fn((r: ArchivedRecording) => {
+        return {
+          messages: jest
+            .fn()
+            .mockReturnValue(of()),
+        };
+      }),
+    };
+  });
+
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+    useRouteMatch: () => ({ url: '/someUrl' }),
+  }));
+
+import { ArchivedRecordingsTable } from '@app/Recordings/ArchivedRecordingsTable';
+import { ArchivedRecording } from '@app/Shared/Services/Api.service';
+
+describe('<ArchivedRecordingsTable />', () => {
+  it('renders correctly', async () => {
+    let tree;
+    await act(async () => {
+      tree = renderer
+      .create(
+        <ServiceContext.Provider value = {defaultServices}>
+          <ArchivedRecordingsTable />
+        </ServiceContext.Provider>)
+    });
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+});
