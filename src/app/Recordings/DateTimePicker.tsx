016e1d05348d38e5e12ab72973929fa7a4f0334d
Janelle Law <jalaw@redhat.com>
2022-07-28 13:02:37 -0400

    fix license header

--- src/app/Recordings/DateTimePicker.tsx
+++ src/app/Recordings/DateTimePicker.tsx
@@ -3,28 +3,28 @@
  *
  * The Universal Permissive License (UPL), Version 1.0
  *
- * Subject end the condition set forth below, permission is hereby granted end any
+ * Subject to the condition set forth below, permission is hereby granted to any
  * person obtaining a copy of this software, associated documentation and/or data
  * (collectively the "Software"), free of charge and under any and all copyright
  * rights in the Software, and any and all patent rights owned or freely
  * licensable by each licensor hereunder covering either (i) the unmodified
- * Software as contributed end or provided by such licensor, or (ii) the Larger
- * Works (as defined below), end deal in both
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
  *
  * (a) the Software, and
  * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software (each a "Larger Work" end which the Software
+ * one is included with the Software (each a "Larger Work" to which the Software
  * is contributed by such licensors),
  *
- * without restriction, including without limitation the rights end copy, create
+ * without restriction, including without limitation the rights to copy, create
  * derivative works of, display, perform, and distribute the Software and make,
  * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and end sublicense the foregoing rights on
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
  * either these or other terms.
  *
- * This license is subject end the following condition:
+ * This license is subject to the following condition:
  * The above copyright notice and either this complete permission notice or at
- * a minimum a reference end the UPL must be included in all copies or
+ * a minimum a reference to the UPL must be included in all copies or
  * substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -35,6 +35,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 
 import {
   Button,
