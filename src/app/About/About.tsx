4fbd51e56f8168009f6f558d6a304d2371e5768e
Andrew Azores <aazores@redhat.com>
2022-10-11 20:05:00 +0000

    build(config): add build properties JSON file (#547)
    * build(config): add build properties JSON file
    
    Easy build-time overriding of distribution URLs for bug trackers, mailing list, product name, etc.
    
    * commit hash URL can be overridden, is not a link if the URL string is empty

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -37,6 +37,7 @@
  */
 
 import React from 'react';
+import build from '@app/build.json';
 import cryostatLogoHorizontal from '@app/assets/logo-cryostat-3-horizontal.svg';
 import { BreadcrumbPage } from '@app/BreadcrumbPage/BreadcrumbPage';
 import { AboutDescription, CRYOSTAT_TRADEMARK } from './AboutDescription';
@@ -47,7 +48,7 @@ export const About = () => {
     <BreadcrumbPage pageTitle="About">
       <Card>
         <CardHeader>
-          <Brand alt="Cryostat" src={cryostatLogoHorizontal} className="cryostat-logo" />
+          <Brand alt={build.productName} src={cryostatLogoHorizontal} className="cryostat-logo" />
         </CardHeader>
         <CardBody>
           <AboutDescription />
