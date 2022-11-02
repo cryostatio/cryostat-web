3c0250d1cfba4711db09093dda4c0072e68caced
Andrew Azores <me@andrewazor.es>
2022-11-02 16:18:15 -0400

    apply prettier formatting

--- src/app/TargetSelect/TargetSelect.tsx
+++ src/app/TargetSelect/TargetSelect.tsx
@@ -242,7 +242,9 @@ export const TargetSelect: React.FunctionComponent<TargetSelectProps> = (props) 
       ].concat(
         targets.map((t: Target) => (
           <SelectOption key={t.connectUrl} value={t} isPlaceholder={false}>
-            {!t.alias || t.alias === t.connectUrl ? `${t.connectUrl}: ${t.jvmId}` : `${t.alias} (${t.connectUrl}): ${t.jvmId}`}
+            {!t.alias || t.alias === t.connectUrl
+              ? `${t.connectUrl}: ${t.jvmId}`
+              : `${t.alias} (${t.connectUrl}): ${t.jvmId}`}
           </SelectOption>
         ))
       ),
