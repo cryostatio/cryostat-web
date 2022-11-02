1bb96ab0afabb52ede0d750c308a9bb4694b4e32
Andrew Azores <me@andrewazor.es>
2022-11-02 17:04:44 -0400

    only display expansion chevron if a target is selected

--- src/app/TargetSelect/TargetSelect.tsx
+++ src/app/TargetSelect/TargetSelect.tsx
@@ -263,12 +263,12 @@ export const TargetSelect: React.FunctionComponent<TargetSelectProps> = (props) 
 
   return (
     <>
-      <Card isExpanded={props.simple ? undefined : isExpanded && selected !== NO_TARGET}>
+      <Card isExpanded={(props.simple || selected === NO_TARGET) ? undefined : isExpanded}>
         <CardHeader
-          onExpand={props.simple ? undefined : onExpand}
-          isToggleRightAligned={props.simple ? undefined : true}
+          onExpand={(props.simple || selected === NO_TARGET) ? undefined : onExpand}
+          isToggleRightAligned={(props.simple || selected === NO_TARGET) ? undefined : true}
           toggleButtonProps={
-            props.simple
+            (props.simple || selected === NO_TARGET)
               ? undefined
               : {
                   id: 'toggle-button1',
@@ -314,7 +314,7 @@ export const TargetSelect: React.FunctionComponent<TargetSelectProps> = (props) 
             {selectOptions}
           </Select>
         </CardBody>
-        {props.simple ? (
+        {(props.simple || selected === NO_TARGET) ? (
           <></>
         ) : (
           <CardExpandableContent>
