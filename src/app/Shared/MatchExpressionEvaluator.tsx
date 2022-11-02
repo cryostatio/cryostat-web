16e2146083624bed3f91bad409833ffc982f321a
Andrew Azores <me@andrewazor.es>
2022-11-02 16:33:41 -0400

    extract SerializedTarget component

--- src/app/Shared/MatchExpressionEvaluator.tsx
+++ src/app/Shared/MatchExpressionEvaluator.tsx
@@ -60,6 +60,7 @@ import {
   InfoCircleIcon,
   WarningTriangleIcon,
 } from '@patternfly/react-icons';
+import { SerializedTarget } from '@app/Shared/SerializedTarget';
 
 export interface MatchExpressionEvaluatorProps {
   inlineHint?: boolean;
@@ -189,13 +190,7 @@ export const MatchExpressionEvaluator: React.FunctionComponent<MatchExpressionEv
           <></>
         )}
         <StackItem>
-          {!!target?.alias && !!target?.connectUrl ? (
-            <CodeBlock>
-              <CodeBlockCode>{JSON.stringify(target, null, 2)}</CodeBlockCode>
-            </CodeBlock>
-          ) : (
-            <NoTargetSelected />
-          )}
+          <SerializedTarget target={target} />
         </StackItem>
       </Stack>
     </>
