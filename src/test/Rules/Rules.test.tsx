754e9e735e5468991e7644af2bf8d7d11a74e0ba
Andrew Azores <aazores@redhat.com>
2022-11-29 10:25:39 -0500

    fix(rules): rename Automatic to Automated

--- src/test/Rules/Rules.test.tsx
+++ src/test/Rules/Rules.test.tsx
@@ -166,7 +166,7 @@ describe('<Rules />', () => {
     expect(modal).toBeInTheDocument();
     expect(modal).toBeVisible();
 
-    const modalTitle = await within(modal).findByText('Upload Automatic Rules');
+    const modalTitle = await within(modal).findByText('Upload Automated Rules');
     expect(modalTitle).toBeInTheDocument();
     expect(modalTitle).toBeVisible();
 
@@ -270,7 +270,7 @@ describe('<Rules />', () => {
     expect(modal).toBeInTheDocument();
     expect(modal).toBeVisible();
 
-    const modalTitle = await within(modal).findByText('Upload Automatic Rules');
+    const modalTitle = await within(modal).findByText('Upload Automated Rules');
     expect(modalTitle).toBeInTheDocument();
     expect(modalTitle).toBeVisible();
 
