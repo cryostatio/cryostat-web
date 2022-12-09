41b5b1b9dc015ebc12a8c19ecb3295e840064e1c
Andrew Azores <aazores@redhat.com>
2022-12-09 10:44:56 -0500

    fixup! move card source into header label

--- src/test/Dashboard/AutomatedAnalysis/AutomatedAnalysisCard.test.tsx
+++ src/test/Dashboard/AutomatedAnalysis/AutomatedAnalysisCard.test.tsx
@@ -283,7 +283,7 @@ describe('<AutomatedAnalysisCard />', () => {
     expect(screen.getByText('Show N/A scores')).toBeInTheDocument();
     expect(screen.getByLabelText('automated-analysis-toolbar')).toBeInTheDocument(); // Toolbar
 
-    expect(screen.getByText(`${mockRecording.name}`)).toBeInTheDocument(); // Active report name
+    expect(screen.getByText(`Active report name=${mockRecording.name}`)).toBeInTheDocument(); // Active report name
     expect(screen.queryByText('Most recent data')).not.toBeInTheDocument(); // Last updated text
 
     expect(
@@ -333,7 +333,7 @@ describe('<AutomatedAnalysisCard />', () => {
     expect(screen.getByText('Show N/A scores')).toBeInTheDocument();
     expect(screen.getByLabelText('automated-analysis-toolbar')).toBeInTheDocument(); // Toolbar
 
-    expect(screen.getByText(`${mockArchivedRecording.name}`)).toBeInTheDocument(); // Archived report name
+    expect(screen.getByText(`Archived report name=${mockArchivedRecording.name}`)).toBeInTheDocument(); // Archived report name
     expect(screen.getByText('Most recent data from 1 day ago.')).toBeInTheDocument(); // Last updated text
 
     expect(
@@ -396,7 +396,7 @@ describe('<AutomatedAnalysisCard />', () => {
     expect(screen.getByText('Show N/A scores')).toBeInTheDocument();
     expect(screen.getByLabelText('automated-analysis-toolbar')).toBeInTheDocument(); // Toolbar
 
-    expect(screen.getByText(automatedAnalysisRecordingName)).toBeInTheDocument(); // Cached report name
+    expect(screen.getByText(`Cached report name=${automatedAnalysisRecordingName}`)).toBeInTheDocument(); // Cached report name
     expect(screen.getByText('Most recent data from 2 days ago.')).toBeInTheDocument(); // Last updated text
 
     expect(
