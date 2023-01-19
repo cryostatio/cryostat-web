5aadd3db1c2c01c76182adbc0be3b4d5ed3cb937
Thuan Vo <thvo@redhat.com>
2023-01-19 15:57:03 -0500

    chore(utils): remove unused functions

--- src/app/Shared/Services/Settings.service.tsx
+++ src/app/Shared/Services/Settings.service.tsx
@@ -86,10 +86,6 @@ export interface Timezone {
   short: string;
 }
 
-export function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
-  return Object.keys(obj).filter((k) => Number.isNaN(+k)) as K[];
-}
-
 export const automatedAnalysisConfigToRecordingAttributes = (
   config: AutomatedAnalysisRecordingConfig
 ): RecordingAttributes => {
