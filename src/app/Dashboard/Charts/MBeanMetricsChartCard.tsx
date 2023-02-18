f5d9941e0c5e2515691ab54ca7cd16abfbd18068
Andrew Azores <aazores@redhat.com>
2023-02-17 19:27:47 -0500

    animate charts

--- src/app/Dashboard/Charts/MBeanMetricsChartCard.tsx
+++ src/app/Dashboard/Charts/MBeanMetricsChartCard.tsx
@@ -126,6 +126,7 @@ const SimpleChart: React.FC<{
       legendPosition={'bottom'}
       themeColor={themeColor}
       width={width}
+      animate={true}
     >
       <ChartAxis tickFormat={(t) => dayjs(t).format('LTS')} fixLabelOverlap />
       <ChartAxis
@@ -260,6 +261,7 @@ const chartKinds: MBeanMetricsChartKind[] = [
           labels={({ datum }) => (datum.x ? `${datum.x}: ${datum.y.toFixed(2)}%` : null)}
           themeColor={themeColor}
           width={width}
+          animate={true}
         />
       );
     },
