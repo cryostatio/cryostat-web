880a5248e3696604bdee35b15c6c6e3e660e21ba
Atif Ali <atali@redhat.com>
2023-08-21 19:07:37 -0400

    fixed Upper case issue

--- src/app/Modal/DeleteWarningUtils.tsx
+++ src/app/Modal/DeleteWarningUtils.tsx
@@ -118,7 +118,7 @@ export const DeleteDashboardLayout: DeleteOrDisableWarning = {
 
 export const ClearDashboardLayout: DeleteOrDisableWarning = {
   id: DeleteOrDisableWarningType.ClearDashboardLayout,
-  title: 'Permanently Clear your Dashboard Layout?',
+  title: 'Permanently clear your Dashboard Layout?',
   label: 'Clear Dashboard Layout',
   description: 'If you click clear, all cards in the current dashboard layout will be removed.',
   ariaLabel: 'Dashboard Layout clear warning',
