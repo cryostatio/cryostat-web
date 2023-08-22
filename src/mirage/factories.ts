d0673a09654886b5f2d2d2bdfd2d1d84da5ba7b7
Thuan Vo <thvo@redhat.com>
2023-08-21 23:55:25 -0400

    feat(mirage): improve discovery route response and fix FormData handler
    Signed-off-by: Thuan Vo <thvo@redhat.com>

--- src/mirage/factories.ts
+++ src/mirage/factories.ts
@@ -23,7 +23,7 @@ export const targetFactory: FactoryDefinition<any> = Factory.extend({
   jvmId: '1234',
   annotations: {
     platform: { 'io.cryostat.demo': 'this-is-not-real' },
-    cryostat: { hello: 'world' },
+    cryostat: { hello: 'world', REALM: 'KubernetesApi' },
   },
 });
 
