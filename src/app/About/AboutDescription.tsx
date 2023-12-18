43c91de53c2b25b978ea26c5a97d5c9de8e4aacb
Andrew Azores <aazores@redhat.com>
2023-12-18 17:16:17 -0500

    fixup! chore(about): clean up for 3.0.0 development

--- src/app/About/AboutDescription.tsx
+++ src/app/About/AboutDescription.tsx
@@ -36,7 +36,7 @@ export const AboutDescription: React.FC = () => {
     if (!cryostatVersion) {
       return;
     }
-    const expr = /^(?<tag>v[0-9]+\\.[0-9]+\\.[0-9]+)(?:-snapshot)$/;
+    const expr = /^(?<tag>v[0-9]+\.[0-9]+\.[0-9]+)(?:-snapshot)?$/;
     const result = cryostatVersion.match(expr);
     if (!result) {
       notificationsContext.warning(
