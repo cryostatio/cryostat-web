87bcffb0c1e0b0cf6a196835c3d421bef290bb96
Andrew Azores <aazores@redhat.com>
2024-03-07 11:47:14 -0500

    fix(rules): correct removal of 'id' field from Rule object

--- src/app/Rules/RulesUploadModal.tsx
+++ src/app/Rules/RulesUploadModal.tsx
@@ -36,7 +36,7 @@ export const parseRule = (file: File): Observable<Rule> => {
     file.text().then((content) => {
       const obj = JSON.parse(content);
       if (obj.id) {
-        obj.remove('id');
+        delete obj.id;
       }
       if (isRule(obj)) {
         return obj;
