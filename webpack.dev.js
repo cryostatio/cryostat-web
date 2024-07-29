551ef5d20e5ce75e9ec87b5a6354c0ee4b0857e4
Thuan Vo <thuan.votann@gmail.com>
2024-07-29 11:43:54 -0700

    build(deps): bump pf5 deps to latest v5

--- webpack.dev.js
+++ webpack.dev.js
@@ -50,9 +50,10 @@ module.exports = merge(common('development'), {
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css'),
           path.resolve(__dirname, 'node_modules/@patternfly/quickstarts/dist/quickstarts.css'),
           path.resolve(__dirname, 'node_modules/@patternfly/react-topology/dist/esm/css'),
+          path.resolve(__dirname, "node_modules/@patternfly/react-topology/node_modules/@patternfly/react-styles/css"),
+          path.resolve(__dirname, "node_modules/@patternfly/react-catalog-view-extension/node_modules/@patternfly/react-styles/css")
         ],
         use: ['style-loader', 'css-loader']
       }
