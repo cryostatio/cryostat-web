72bb289b13b020c1a05f5b791e6b1ec6aa42f268
Andrew Azores <aazores@redhat.com>
2025-04-15 16:27:18 -0400

    prettier

--- src/test/Settings/Settings.test.tsx
+++ src/test/Settings/Settings.test.tsx
@@ -107,7 +107,6 @@ jest.mock('@app/Settings/Config/Theme', () => ({
   } as UserSetting,
 }));
 
-
 jest.mock('@app/Settings/Config/Topology', () => ({
   Topology: {
     titleKey: 'SETTINGS.TOPOLOGY.TITLE',
