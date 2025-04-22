81cef566e072d2971361b11561f02668b5ec97c4
Andrew Azores <aazores@redhat.com>
2025-04-22 16:25:09 -0400

    feat(rules): implement Edit button for in-place editing of existing Rule

--- src/app/CreateRecording/utils.ts
+++ src/app/CreateRecording/utils.ts
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { TemplateType } from '@app/Shared/Services/api.types';
+import { EventTemplateIdentifier } from './types';
+
 export const RecordingNamePattern = /^[\w_]+$/;
 
 export const DurationPattern = /^[1-9][0-9]*$/;
@@ -20,3 +23,17 @@ export const DurationPattern = /^[1-9][0-9]*$/;
 export const isRecordingNameValid = (name: string) => RecordingNamePattern.test(name);
 
 export const isDurationValid = (duration: number) => DurationPattern.test(`${duration}`);
+
+export const templateFromEventSpecifier = (eventSpecifier: string): EventTemplateIdentifier | undefined => {
+  const regex = /^template=([a-zA-Z0-9]+)(?:,type=([a-zA-Z0-9]+))?$/im;
+  if (eventSpecifier && regex.test(eventSpecifier)) {
+    const parts = regex.exec(eventSpecifier);
+    if (parts) {
+      return {
+        name: parts[1],
+        type: parts[2] as TemplateType,
+      };
+    }
+  }
+  return undefined;
+};
