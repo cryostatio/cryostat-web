d258f92822daf07a800b250b8a0925cac380daa0
Andrew Azores <me@andrewazor.es>
2025-11-17 14:12:36 -0500

    remove reference to old-style automated analysis card

--- src/app/QuickStarts/quickstarts/settings-quickstart.tsx
+++ src/app/QuickStarts/quickstarts/settings-quickstart.tsx
@@ -84,14 +84,9 @@ The **Notifications & messages** tab allows you to configure the notifications a
         description: `
 The **Dashboard** tab allows you to configure settings for the various Dashboard Cards that you can add to the Dashboard.
 
-The **automated analysis** Dashboard Card allows you to automatically start an analysis on the Recording with a click of a button. You can configure the Recording that is started by this card.
-
 1. From the list of settings tabs, click [Dashboard]{{highlight settings-dashboard-tab}}.
-2. Configure the **Automated analysis Recording configuration** settings.
-3. Configure the **Dashboard metrics configuration** settings.
+2. Configure the **Dashboard metrics configuration** settings.
 
-[When using the **automated analysis card**, make sure the **Event Template** is compatible with the Target JVM.]{{admonition warning}}
-[Setting both an infinite maximum size and age may result in an **Out Of Memory** error during report generation.]{{admonition caution}}
 `,
       },
       {
