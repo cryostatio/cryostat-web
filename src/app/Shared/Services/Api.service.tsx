6abf563600fd1eb73cc1f1e77140196923695635
Andrew Azores <me@andrewazor.es>
2026-01-07 11:09:26 -0500

    empty state if async-profiler not detected in target

--- src/app/Shared/Services/Api.service.tsx
+++ src/app/Shared/Services/Api.service.tsx
@@ -1984,7 +1984,7 @@ export class ApiService {
     );
   }
 
-  getAsyncProfilerStatus(target: Target): Observable<AsyncProfilerStatus> {
+  getAsyncProfilerStatus(target: Target, suppressNotifications = false): Observable<AsyncProfilerStatus> {
     return this.doGet<{
       currentProfile: {
         id: string;
@@ -1994,8 +1994,16 @@ export class ApiService {
       };
       status: string;
       availableEvents: string[];
-    }>(`targets/${target.id}/async-profiler/status`, 'beta').pipe(
+    }>(`targets/${target.id}/async-profiler/status`, 'beta', undefined, suppressNotifications).pipe(
       map((s) => ({ ...s, status: s['status'] === 'RUNNING' })),
+    );
+  }
+
+  isAsyncProfilerSupported(target: Target): Observable<boolean> {
+    return this.getAsyncProfilerStatus(target, true).pipe(
+      map((_) => true),
+      catchError((_) => of(false)),
+      first(),
     );
   }
 
